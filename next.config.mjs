b1781bd0b1c10e07a7865b43256f31aa34d2d6a0
kelvinguchu <kelvinguchu5@gmail.com>
2026-01-30 17:35:20 +0300

    feat: update image remote patterns configuration in Next.js setup

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,13 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+    images: {
+      remotePatterns: [{ protocol: 'https', hostname: '**' }],
+      formats: ['image/webp', 'image/avif', 'image/jpeg', 'image/png', 'image/jpg'],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      minimumCacheTTL: 2678400,
+    },
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
