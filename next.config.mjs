1977841d936766fa507a9b2dea39c61d306410e5
kelvinguchu <kelvinguchu5@gmail.com>
2026-01-30 17:39:22 +0300

    feat: remove 'image/jpg' format from Next.js image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
     images: {
       remotePatterns: [{ protocol: 'https', hostname: '**' }],
-      formats: ['image/webp', 'image/avif', 'image/jpeg', 'image/png', 'image/jpg'],
+      formats: ['image/webp', 'image/avif', 'image/jpeg', 'image/png'],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
       minimumCacheTTL: 2678400,
